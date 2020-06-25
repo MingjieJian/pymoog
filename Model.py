b6d3ae20db70091b639d389b1ef5ab365ce252d2
Mingjie Jian <ssaajianmingjie@gmail.com>
2020-06-26 00:25:01 +0900

    Mid commit during MG

--- Model.py
+++ Model.py
@@ -4,43 +4,11 @@ import re
 import numpy as np
 import os
 
-def v2fn(value_list):
+MOOGrun_path = '{}/.pymoog/rundir/'.format(os.environ['HOME'])
+
+def KURUCZ_download(teff, logg, m_h, save_name=None):
     '''
-    The function to translate the value into name, according to the grids of Meszaros (2016). The values input have to be the same as the grid.
-
-    Parameters
-    ----------
-    value_list : list, [m_h, c_m, alpha_m, teff, logg]
-        List containing the input parameters.
-
-    Returns
-    ----------
-    fn_list : list
-        List of strings to be put into the path of atmosphere model.
-    '''
-    fn_list = []
-    for value in value_list[:-2]:
-        if value < 100:
-            fn = '{:02d}'.format(int(math.ceil(abs(value)*10)))
-        else:
-            fn = '{:4}'.format(value)
-        if value < 0:
-            fn = 'm'+fn
-        elif value < 100:
-            fn = 'p'+fn
-        fn_list.append(fn)
-    for value in value_list[-2:]:
-        if value < 100:
-            fn = '{:02d}'.format(int(abs(value)*10))
-        else:
-            fn = '{:4}'.format(value)
-        fn_list.append(fn)
-    return fn_list
-
-
-def KURUCZ_APOGEE_download(teff, logg, m_h, c_m=0, alpha_m=0, to_path='./'):
-    '''
-    Download the ATLAS9-APOGEE grid file.
+    Download the Kurucz ATLAS9 model using pyasl.
 
     Parameters
     ----------
@@ -50,10 +18,6 @@ def KURUCZ_APOGEE_download(teff, logg, m_h, c_m=0, alpha_m=0, to_path='./'):
         logg value of the model
     m_h : float
         [M/H] value (overall metallicity) of the model
-    c_m : float
-        [C/M] value of the model
-    alpha_m : float
-        [alpha/M] value of the model
     to_path : str
         The path to save the model file.
 
@@ -61,28 +25,22 @@ def KURUCZ_APOGEE_download(teff, logg, m_h, c_m=0, alpha_m=0, to_path='./'):
     ----------
     to_name : str
         The path and name of saved model.
-    dl_situtation : boolen
-        Download situation, True if succeeded, False if falied.
 
     '''
-    vlist = [m_h, c_m, alpha_m, teff, logg]
-    vlist = v2fn(vlist) + [to_path]
-    file_name = '{5}am{0}c{1}o{2}t{3}g{4}v20.mod'.format(*vlist)
 
     #The model is invoked here
-    model = pyasl.getKuruczModel(teff,logg,m_h)
+    model = pyasl.getKuruczModel(teff, logg, m_h)
 
-    outf = open(file_name,"w")
+    if save_name == None:
+        output_file_name = MOOGrun_path + '/model.mod'
+    output_file = open(output_file_name,"w")
 
     for line in model:
-        outf.write(line)
-        outf.write("\n")
+        output_file.write(line + "\n")
 
-    outf.close()
+    output_file.close()
 
-    k_model_path, cv = KURUCZ_APOGEE_convert(file_name)
-
-    return k_model_path,cv
+    return MOOGrun_path + '/model.mod'
 
 
 def KURUCZ_APOGEE_convert(file_path, vmicro=2.0, abun_change=None):
