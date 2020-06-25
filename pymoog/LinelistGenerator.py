0bd47ed566987f41154856806abcf2646f7d1e06
Kruthi <48632511+Kruthi24@users.noreply.github.com>
2020-06-26 04:00:33 +0530

    Updating LinelistGenerator.py

--- pymoog/LinelistGenerator.py
+++ pymoog/LinelistGenerator.py
@@ -4,13 +4,6 @@ import mendeleev as md
 import re
 import sys
 
-def main():
-   init_linelist_name = sys.argv[1]
-   out_linelist_name = sys.argv[2]
-
-if __name__ == "__main__":
-    main()
-
 # Convert the element column to element specics
 
 def get_isotope_list(string):
@@ -31,7 +24,6 @@ def get_isotope_list(string):
     return isotope_list
 
 def element2index(string_all):
-    # print(string_all)
     string, isotope_string = string_all.split(',')
     isotope_string = isotope_string[-12:]
     element_string, ion_stage = string.split(' ')
@@ -73,7 +65,10 @@ def element2index(string_all):
         element_indices_string = '{:2.0f}' + '{:02.0f}'*(len(element_indices)-1) + '.0' + '{:02.0f}'*len(isotope_list)
         return element_indices_string.format(*element_indices, *isotope_list)
 
-def get_diss_energy(ele_index, diss_energy_pd=diss_energy):
+def get_diss_energy(ele_index):
+    diss_energy = pd.read_csv('./files/dissociation_energy_list.csv')
+    diss_energy['diss_energy(eV)'] = diss_energy['dissociation_energy (kJ/mol)'] / 96.485
+    diss_energy_pd = diss_energy
     ele_index = np.floor(float(ele_index))
     try:
         diss_energy_value = diss_energy_pd.loc[diss_energy_pd['element_index'] == ele_index, 'diss_energy(eV)'].values[0]
@@ -82,21 +77,35 @@ def get_diss_energy(ele_index, diss_energy_pd=diss_energy):
         return np.nan
     
     
-def vald2moog_format(init_linelist_name):
+def vald2moog_format(init_linelist_name,out_linelist_name,lim1, lim2):
     '''
-    Transform VALD linelist into MOOG format.
+    Transform VALD linelist into MOOG format for a given wavlength range.
     '''
+    vald_init = pd.read_csv(init_linelist_name,skiprows=2, skipfooter=107, usecols=range(9), engine = 'python' ,
+                                names=['element', 'wav', 'EP', 'loggf', 'rad_damp', 'Stark_damp', 'Walls_damp', 'Lande_factor', 'Comment'])
+    vald_init = vald_init[(vald_init['wav'] >= lim1) & (vald_init['wav'] <=lim2)]
 
-    vald_init = pd.read_csv(init_linelist_name,skiprows=2, skipfooter=107, usecols=range(9), names=['element', 'wav', 'EP', 'loggf', 'rad_damp', 'Stark_damp', 'Walls_damp', 'Lande_factor', 'Comment'])
-    vald_init = vald_init[:50]
     vald_init['element_all'] = vald_init[['element', 'Comment']].apply(lambda x: ', '.join(x), axis=1)
-
     vald_init['element_index'] = vald_init['element_all'].map(element2index)
     vald_init['diss_energy'] = vald_init['element_index'].map(get_diss_energy)
 
+    
     vald_out = vald_init[['wav', 'element_index', 'EP', 'loggf', 'Walls_damp', 'diss_energy']]
+    format3 = "{0:.3f}".format
+    format4 = "{0:.4f}".format
+    pd.options.mode.chained_assignment = None
+    vald_out[['wav','EP']] = vald_out[['wav', 'EP']].applymap(format4)
+    vald_out[['loggf', 'Walls_damp', 'diss_energy']] = vald_out[['loggf', 'Walls_damp','diss_energy']].applymap(format3)
+    vald_out.to_csv(out_linelist_name, index = False, sep='\t')
 
-    return vald_out
+  
 
-diss_energy = pd.read_csv('pymoog/files/dissociation_energy_list.csv')
-diss_energy['diss_energy(eV)'] = diss_energy['dissociation_energy (kJ/mol)'] / 96.485
+def main():
+   init_linelist_name = "../vald_init"
+   out_linelist_name = './vald_output'
+   lambda1 = float(sys.argv[1])
+   lambda2 = float(sys.argv[2])
+   vald2moog_format (init_linelist_name, out_linelist_name,lambda1, lambda2)
+
+if __name__ == "__main__":
+    main()
