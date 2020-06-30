6343d71802dd0f5e62e4236e023228aa349715b6
Mingjie Jian <ssaajianmingjie@gmail.com>
2020-06-30 15:43:45 +0900

    RTD line data test - added RTD judge back

--- setup.py
+++ setup.py
@@ -2,44 +2,44 @@ import subprocess
 import os
 import setuptools
 
+if os.environ.get('READTHEDOCS') != 'True':
+    # Define MOOGMODELING_path. This path will store the code of moog and any other temporary files in the calculation.
+    MOOGMODELING_path = '{}/.pymoog/'.format(os.environ['HOME'])
 
-# Define MOOGMODELING_path. This path will store the code of moog and any other temporary files in the calculation.
-MOOGMODELING_path = '{}/.pymoog/'.format(os.environ['HOME'])
+    # Create the folder according to MOOGMODELING_path
+    if not(os.path.isdir(MOOGMODELING_path)):
+        os.mkdir(MOOGMODELING_path)
 
-# Create the folder according to MOOGMODELING_path
-if not(os.path.isdir(MOOGMODELING_path)):
-    os.mkdir(MOOGMODELING_path)
+    # Create the folder for calculation
+    if not(os.path.isdir(MOOGMODELING_path + 'rundir')):
+        os.mkdir(MOOGMODELING_path + 'rundir')
+        
+    # Copy the files folder into working directory
+    if not(os.path.isdir(MOOGMODELING_path + 'files')):
+        os.mkdir(MOOGMODELING_path + 'files')
+    rm_status = subprocess.run(['rm', '-r', MOOGMODELING_path + 'files/'], stdout=subprocess.PIPE)
+    cp_status = subprocess.run(['cp', '-r', 'pymoog/files', MOOGMODELING_path + 'files'], stdout=subprocess.PIPE)
 
-# Create the folder for calculation
-if not(os.path.isdir(MOOGMODELING_path + 'rundir')):
-    os.mkdir(MOOGMODELING_path + 'rundir')
-    
-# Copy the files folder into working directory
-if not(os.path.isdir(MOOGMODELING_path + 'files')):
-    os.mkdir(MOOGMODELING_path + 'files')
-rm_status = subprocess.run(['rm', '-r', MOOGMODELING_path + 'files/'], stdout=subprocess.PIPE)
-cp_status = subprocess.run(['cp', '-r', 'pymoog/files', MOOGMODELING_path + 'files'], stdout=subprocess.PIPE)
+    # Copy the moog_nosm folder to MOOGMODELING_path; if the folder already exist it will be removed first.
+    if os.path.isdir(MOOGMODELING_path + 'moog_nosm'):
+        rm_status = subprocess.run(['rm', '-r', MOOGMODELING_path + 'moog_nosm'], stdout=subprocess.PIPE)
+    cp_status = subprocess.run(['cp', '-r', 'moog_nosm', MOOGMODELING_path + 'moog_nosm'], stdout=subprocess.PIPE)
 
-# Copy the moog_nosm folder to MOOGMODELING_path; if the folder already exist it will be removed first.
-if os.path.isdir(MOOGMODELING_path + 'moog_nosm'):
-    rm_status = subprocess.run(['rm', '-r', MOOGMODELING_path + 'moog_nosm'], stdout=subprocess.PIPE)
-cp_status = subprocess.run(['cp', '-r', 'moog_nosm', MOOGMODELING_path + 'moog_nosm'], stdout=subprocess.PIPE)
+    # Check the permission of ./install
+    chmod_subp = subprocess.run(['chmod', '775', './install.sh'], cwd=MOOGMODELING_path+'moog_nosm/moog_nosm_FEB2017/', stdout=subprocess.PIPE)
 
-# Check the permission of ./install
-chmod_subp = subprocess.run(['chmod', '775', './install.sh'], cwd=MOOGMODELING_path+'moog_nosm/moog_nosm_FEB2017/', stdout=subprocess.PIPE)
+    # Check if gfortran is installed in the system.
+    # Not done yet
 
-# Check if gfortran is installed in the system.
-# Not done yet
+    # Run the MOOGMODELING_path/moog_nosm/moog_nosm_FEB2017/install.sh script
+    install = subprocess.run(['bash', './install.sh', 'Linux'], cwd=MOOGMODELING_path+'moog_nosm/moog_nosm_FEB2017/', encoding='UTF-8', stdout=subprocess.PIPE)
+    print(install.stdout)
 
-# Run the MOOGMODELING_path/moog_nosm/moog_nosm_FEB2017/install.sh script
-install = subprocess.run(['bash', './install.sh', 'Linux'], cwd=MOOGMODELING_path+'moog_nosm/moog_nosm_FEB2017/', encoding='UTF-8', stdout=subprocess.PIPE)
-print(install.stdout)
-
-# Check if MOOG and MOOGSILENT is in the folder
-if not(os.path.isfile(MOOGMODELING_path+'moog_nosm/moog_nosm_FEB2017/MOOG')) or not(os.path.isfile(MOOGMODELING_path+'moog_nosm/moog_nosm_FEB2017/MOOG')):
-    raise ValueError("MOOG is not installed correctly!")
-else:
-    print('Successfully installed MOOG!')
+    # Check if MOOG and MOOGSILENT is in the folder
+    if not(os.path.isfile(MOOGMODELING_path+'moog_nosm/moog_nosm_FEB2017/MOOG')) or not(os.path.isfile(MOOGMODELING_path+'moog_nosm/moog_nosm_FEB2017/MOOG')):
+        raise ValueError("MOOG is not installed correctly!")
+    else:
+        print('Successfully installed MOOG!')
     
     
 with open("README.md", "r") as fh:
