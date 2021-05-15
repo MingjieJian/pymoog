e1ac14266b77dad49f361fbc422650a58f208691
Mingjie Jian <ssaajianmingjie@gmail.com>
2021-05-15 18:34:13 +0900

    feat(cog): add get_line_status to cog
    cog now can get line status from the result of MOOG.

--- pymoog/cog.py
+++ pymoog/cog.py
@@ -8,7 +8,7 @@ MOOG_path = '{}/.pymoog/moog_nosm/moog_nosm_NOV2019/'.format(private.os.environ[
 MOOG_file_path = '{}/.pymoog/files/'.format(private.os.environ['HOME'])
 
 class cog(rundir_num.rundir_num):
-    def __init__(self, teff, logg, m_h, line_list, cog_low=-7.5, cog_up=-3.5, cog_step=0.05, lp_step=0):
+    def __init__(self, teff, logg, m_h, line_list, vmicro=2., cog_low=-7.5, cog_up=-3.5, cog_step=0.05, lp_step=0):
         '''
         Initiate a cog Instance and read the parameters. 
         line_list muse be provided with suffix of '.list', and it can only contain one line.
@@ -23,8 +23,10 @@ class cog(rundir_num.rundir_num):
             [M/H] value (overall metallicity) of the model
         line_list : pandas.DataFrame
             The dataframe containting the line information.
+        vmicro : float, default 2
+            The microtrubulance velocity of the synthesized spectra (this is different from the v_micro of the atmosphere model which is always 2)
         cog_low : float, default -7.5
-            The log(W/lambda) lower limit for the cog. If larger than -5 then it will be set as -5 in MOOG.
+            The log(W/lambda) lower limit for the cog. If larger than -6.5 then it will be set as -6.5 in MOOG.
         cog_up : float, default -3.5
             The log(W/lambda) upper limit for the cog. When larger than -3 step jump of cog will present.
         cog_step : float, default 0.05
@@ -41,6 +43,7 @@ class cog(rundir_num.rundir_num):
         self.cog_up = cog_up
         self.cog_step = cog_step
         self.lp_step = lp_step
+        self.vmicro = vmicro
         
     def prepare_file(self, model_file=None, model_type='moog', abun_change=None, molecules=None, atmosphere=1, lines=1):
         '''
@@ -67,7 +70,7 @@ class cog(rundir_num.rundir_num):
         
         if model_file == None:
             # Model file is not specified, will download Kurucz model according to stellar parameters.
-            model.interpolate_model(self.teff, self.logg, self.m_h, abun_change=abun_change, molecules=molecules, to_path=self.rundir_path + 'model.mod')
+            model.interpolate_model(self.teff, self.logg, self.m_h, abun_change=abun_change, molecules=molecules, to_path=self.rundir_path + 'model.mod', vmicro=self.vmicro)
             self.model_file = 'model.mod'
         else:
             # Model file is specified; record model file name and copy to working directory.
@@ -78,11 +81,12 @@ class cog(rundir_num.rundir_num):
                 model.KURUCZ_convert(model_path=model_file, abun_change=abun_change, model_type=model_type[7:], molecules=molecules, converted_model_path=self.rundir_path + 'model.mod')
                 self.model_file = 'model.mod'
 
-        # Linelist file is specified; record linelist file name and copy to working directory.
+        # Linelist file must be specified; record linelist file name and copy to working directory.
         # self.line_list = line_data.read_linelist(self.line_list)
         # line_data.save_linelist(self.line_list, self.rundir_path + '/line.list')
         private.subprocess.run(['cp', self.line_list, self.rundir_path+'/line.list'], encoding='UTF-8', stdout=private.subprocess.PIPE)
         self.line_list_name = 'line.list'
+        self.line_df = line_data.read_linelist(self.line_list)
             
         # Create parameter file.
         self.create_para_file(atmosphere=atmosphere, lines=lines)    
@@ -186,3 +190,112 @@ class cog(rundir_num.rundir_num):
         
         if unlock:
             self.unlock()
+            
+    def get_line_status(self, input_loggf=private.np.nan, plot='none'):
+        
+        '''
+        Derive spectral line status (Linear/Saturated/Damped) of the line input to cog using input loggf value.
+        
+        Parameters
+        ----------
+        input_loggf : float, default np.nan
+            The input loggf for determining the line status. If not specified, will use the loggf of the line in cog.
+        plot : str, default 'none'
+            Plot status. If 'none', nothing will be plot; if 'cog', then cog will be plot; if 'all', then cog, the first and second derivative will be plot.    
+
+        Returns
+        ---------
+        self.line_status : list
+            List containing the line status: ['<Linear/Saturated/Damped>', <loggf separation between linear/saturated region>, <loggf separation between saturated/damped region>]
+        '''
+        
+        # Check whether input_loggf is specified. If not then use line loggf
+        if private.np.isnan(input_loggf):
+            input_loggf = self.line_df['loggf'].values[0]
+        
+        logrw_poly = private.np.polyfit(self.loggf,self.logrw, 20)
+        if private.np.any(private.np.abs(self.logrw - private.np.polyval(logrw_poly, self.loggf)) > 0.05):
+            private.warnings.warn('Fitting to the cog have an discrepancy larger than 0.05 in some point, please check the cog in detail if necessary')
+        logrw_1d = private.np.polyval(private.np.polyder(logrw_poly), self.loggf)
+        logrw_2d = private.np.polyval(private.np.polyder(private.np.polyder(logrw_poly)), self.loggf)
+        # Determine the length of logrw_2d to be set to 0.
+        zero_length = int(private.np.ceil(2/151*len(self.loggf)))
+        if zero_length < 2:
+            zero_length = 2
+        logrw_2d[:zero_length] = 0
+        logrw_2d[-zero_length:] = 0
+
+        sat_loggf_sep = self.loggf[private.np.argmin(logrw_2d)]
+        dam_loggf_sep = self.loggf[private.np.argmax(logrw_2d)]
+        sat_logrw_val = self.logrw[private.np.argmin(logrw_2d)]
+        dam_logrw_val = self.logrw[private.np.argmax(logrw_2d)]
+
+        # Check the values of seperations
+        if (sat_loggf_sep > dam_loggf_sep and sat_logrw_val > dam_logrw_val) or logrw_1d[private.np.argmax(logrw_2d)] > 0.8:
+            # Damping part not present
+            dam_loggf_sep = private.np.nan
+            dam_logrw_val = private.np.nan
+
+        if logrw_1d[private.np.argmin(logrw_2d)] > 0.8:
+            # Saturation part not present
+            sat_loggf_sep = private.np.nan
+            sat_logrw_val = private.np.nan
+
+        # Derive the status of the line
+        if input_loggf >= max(self.loggf) or input_loggf <= min(self.loggf):
+            line_status = 'Unknown'
+        else:
+            if private.np.isnan(sat_loggf_sep) and private.np.isnan(dam_loggf_sep):
+                if private.np.polyval(private.np.polyder(logrw_poly), input_loggf) >= 0.8:
+                    line_status = 'Linear'
+                else:
+                    line_status = 'Unknown'
+            elif private.np.isnan(dam_loggf_sep):
+                if input_loggf <= sat_loggf_sep:
+                    line_status = 'Linear'
+                else:
+                    line_status = 'Saturated'
+            else:
+                if input_loggf <= sat_loggf_sep:
+                    line_status = 'Linear'
+                elif input_loggf <= dam_loggf_sep:
+                    line_status = 'Saturated'
+                else:
+                    line_status = 'Damped'
+
+        if plot == 'cog':
+            private.plt.figure(figsize=(6,3), dpi=100)
+            private.plt.plot(self.loggf, self.logrw)
+            private.plt.ylabel('$\log{(W/\lambda)}$')
+            private.plt.xlabel("$\log{(gf)}$")
+            
+            private.plt.axvline(sat_loggf_sep, ls='--', c='C3', label='linear-stutared separation')
+            private.plt.axvline(dam_loggf_sep, ls='--', c='C4', label='stutared-damped separation')
+            private.plt.axvline(input_loggf, c='C5', label='Input $\log{(gf)}$')
+            private.plt.legend(fontsize=7, loc=4)
+            private.plt.title('Line status: {}'.format(line_status))
+        elif plot == 'all':
+            private.plt.figure(figsize=(6,3*3), dpi=100)
+
+            ax1 = private.plt.subplot(311)
+            private.plt.plot(self.loggf, self.logrw)
+            private.plt.ylabel('$\log{(W/\lambda)}$')
+
+            ax2 = private.plt.subplot(312)
+            private.plt.plot(self.loggf, logrw_1d)
+            private.plt.ylabel("$[\log{(W/\lambda)}]'$")
+
+            ax3 = private.plt.subplot(313)
+            private.plt.plot(self.loggf, logrw_2d)
+            private.plt.ylabel("$[\log{(W/\lambda)}]''$")
+            private.plt.xlabel("$\log{(gf)}$")
+
+            for ax in [ax1, ax2, ax3]:
+                ax.axvline(sat_loggf_sep, ls='--', c='C3', label='linear-stutared separation')
+                ax.axvline(dam_loggf_sep, ls='--', c='C4', label='stutared-damped separation')
+                ax.axvline(input_loggf, c='C5', label='Input $\log{(gf)}$')
+
+            ax1.set_title('Line status: {}'.format(line_status))
+            ax1.legend(fontsize=7, loc=4)
+            
+        self.line_status = [line_status, sat_loggf_sep, dam_loggf_sep]
