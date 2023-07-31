8052cf3b6453208520e12ee201e74c981b31ea25
Mingjie Jian <jian-mingjie@outlook.com>
2023-07-31 23:58:43 +0900

    feat(line_data): add depth blending ratio

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ with open("README.md", "r") as fh:
 if os.environ.get('READTHEDOCS') != 'True':
     setuptools.setup(
         name='pymoog',
-        version='0.1.1',
+        version='0.1.2',
         description='The python wrapper to run LTE spectra synthesis code MOOG.',
         long_description=long_description,
         long_description_content_type="text/markdown",
