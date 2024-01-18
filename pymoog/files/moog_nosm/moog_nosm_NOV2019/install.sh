b2cc8ba1952fd41c309450e85da9c2817ee50bc6
Mingjie Jian <jian-mingjie@outlook.com>
2024-01-19 06:09:13 +0900

    renew install.sh - 3

--- pymoog/files/moog_nosm/moog_nosm_NOV2019/install.sh
+++ pymoog/files/moog_nosm/moog_nosm_NOV2019/install.sh
@@ -10,6 +10,7 @@
 # Note that the code will break the path to multiple lines if it is longer than 60.
 
 path=`pwd`'/'
+path='asdfadsfasdfweadfasfcasdfasdfaewfawsefasdfawefagasdfgasdfawefawefawefawfdasdgasgawsgewfaew'
 
 chunk_length=55
 
@@ -23,43 +24,44 @@ if [ ${#path} -gt $chunk_length ]; then
 		replacement_string+=" //"
         replacement_string+=$'\\n'
     done
+	replacement_string=${replacement_string::-4}
 else 
 	replacement_string="     .  '${path}'"
 fi
 
 sed -i "22s#.*#$replacement_string#" Moog.f
-sed -i "22s#.*#$replacement_string#" Moogsilent.f
+# sed -i "22s#.*#$replacement_string#" Moogsilent.f
 
-# Change the machine to user specified type.
-machine='None'
-while [ $machine == 'None' ]
-do
-	machine=`uname`
-	if [[ -z "$machine" ]] || [ $machine == 'Linux' ] 
-	then
-		machine='pcl'
-		break
-	elif [ $machine == 'Darwin' ]
-	then
-		machine='mac'
-		break
-	else 
-		machine='pcl'
-	fi
-done
+# # Change the machine to user specified type.
+# machine='None'
+# while [ $machine == 'None' ]
+# do
+# 	machine=`uname`
+# 	if [[ -z "$machine" ]] || [ $machine == 'Linux' ] 
+# 	then
+# 		machine='pcl'
+# 		break
+# 	elif [ $machine == 'Darwin' ]
+# 	then
+# 		machine='mac'
+# 		break
+# 	else 
+# 		machine='pcl'
+# 	fi
+# done
 
-# machine='pcl'
-sed -i "29s/'.*'/'$machine'/" Moog.f
-sed -i "29s/'.*'/'$machine'/" Moogsilent.f
+# # machine='pcl'
+# sed -i "29s/'.*'/'$machine'/" Moog.f
+# sed -i "29s/'.*'/'$machine'/" Moogsilent.f
 
-# Install MOOG and MOOGSILENT
+# # Install MOOG and MOOGSILENT
 
-if [ $machine == 'pcl' ]
-then
-	make -f Makefile.rh64 
-	make -f Makefile.rh64silent 
-elif [ $machine == 'mac' ]
-then
-	make -f Makefile.maclap 
-	make -f Makefile.maclapsilent 
-fi
+# if [ $machine == 'pcl' ]
+# then
+# 	make -f Makefile.rh64 
+# 	make -f Makefile.rh64silent 
+# elif [ $machine == 'mac' ]
+# then
+# 	make -f Makefile.maclap 
+# 	make -f Makefile.maclapsilent 
+# fi
